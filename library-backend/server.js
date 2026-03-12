c64c06e6de33a3c99109cca52f00e42a614216c9
Prithvi Nawadiya <nawadiyaprithvi@gmail.com>
2026-03-12 11:44:06 +0530

    chore: minor update

--- library-backend/server.js
+++ library-backend/server.js
@@ -31,4 +31,6 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+});
